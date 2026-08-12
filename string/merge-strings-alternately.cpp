9bc8e3c1f44839d258629fcd792f00a51215b448
Dushyant <dushyant.ug24@nsut.ac.in>
2026-08-12 23:48:47 +0530

    LC-merge-strings-alternately [Runtime: N/A; Memory: N/A]

--- string/merge-strings-alternately.cpp
+++ string/merge-strings-alternately.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int left = 0;
-        int right = 0;
-        string ans = "";
-
-        while (left < word1.length() || right < word2.length()) {
-
-            if (left < word1.length()) {
-                ans += word1[left];
-                left++;
-            }
-
-            if (right < word2.length()) {
-                ans += word2[right];
-                right++;
-            }
+        int pointer = 0;
+        string result = "";
+        
+        while(pointer < word1.length() && pointer < word2.length()){
+            result= result + word1[pointer] + word2[pointer];
+            pointer++;
         }
-
-        return ans;
+        while(pointer < word1.length()){
+            result= result + word1[pointer];
+            pointer++;
+        }
+        while(pointer < word2.length()){
+            result= result + word2[pointer];
+            pointer++;
+        }
+        return result;
     }
 };
