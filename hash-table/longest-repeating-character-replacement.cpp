55ac55168bafdb078a76b71b986389db8310f4e0
Dushyant <dushyant.ug24@nsut.ac.in>
2026-08-13 23:59:36 +0530

    LC-longest-repeating-character-replacement [Runtime: N/A; Memory: N/A]

--- hash-table/longest-repeating-character-replacement.cpp
+++ hash-table/longest-repeating-character-replacement.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int characterReplacement(std::string s, int k) {
+        unordered_map<char, int> count;
         int res = 0;
-        unordered_set<char> charSet(s.begin(), s.end());
 
-        for (char c : charSet) {
-            int count = 0, l = 0;
-            for (int r = 0; r < s.size(); r++) {
-                if (s[r] == c) {
-                    count++;
-                }
-
-                while ((r - l + 1) - count > k) {
-                    if (s[l] == c) {
-                        count--;
-                    }
-                    l++;
-                }
-
-                res = std::max(res, r - l + 1);
+        int l = 0, maxf = 0;
+        for (int r = 0; r < s.size(); r++) {
+            count[s[r]]++;
+            maxf = max(maxf, count[s[r]]);
+            while ((r - l + 1) - maxf > k) {
+                count[s[l]]--;
+                l++;
             }
+            res = max(res, r - l + 1);
         }
+
         return res;
     }
 };
